4ac6c3f2c424d0994305e5683a62c998fe0801c4
deeCodeMonkey <deidra108@gmail.com>
2018-04-24 15:48:31 -0600

    user auth flow with cookies, passport, express

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,13 +1,17 @@
-﻿const passport = require("passport");
+﻿const passport = require('passport');
 
-module.exports = (app) => {
+module.exports = app => {
 
-    //route for google auth - passport to use strategy called google
+    //route for google auth - passport to use strategy called google to create new user
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
     }));
 
-
     app.get('/auth/google/callback', passport.authenticate('google'));
 
-}
+    app.get('/api/current_user', (req, res) => {
+        //route handler to test and see if user is present
+        res.send(req.user);
+    });
+
+};
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,28 @@
-﻿const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys");
+﻿const keys = require('../config/keys');
+
+const mongoose = require('mongoose');
+//one arg to fetch collection, two args to modify collection
+const User = mongoose.model('users');
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+//place unique user id into cookie (encrypt)
+//turn mongoose id to cookie id
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+//get unique user id (de-encrypt) from cookie
+//turn cookie id to mongoose id
+//becomes a user model instance added to req object as 'req.user'
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then( user => {
+            done(null, user);
+        });
+});
+
 
 //register Google for authentication
 passport.use(new GoogleStrategy({
@@ -9,9 +31,23 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
 },
     (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken, 'access token');
-        console.log(refreshToken, 'refresh token');
-        console.log(profile, 'profile');
+        //console.log(accessToken, 'access token');
+        //console.log(refreshToken, 'refresh token');
+        //console.log(profile, 'profile');
+
+        //find if user already exists
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    //"Done" lets passport know auth is completed, with "null" means no error and user record as "existingUser"
+                    done(null, existingUser);
+                } else {
+                    //create new user
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then((user) => done(null, user));
+                }
+            });
     })
 );
 
