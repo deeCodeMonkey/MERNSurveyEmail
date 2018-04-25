42b2579ff4a63cc0e6f36d7253280a7cbdd928c5
deeCodeMonkey <deidra108@gmail.com>
2018-04-24 18:15:04 -0600

    set keys

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,7 +9,17 @@ module.exports = app => {
 
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+        //passport also provides/attaches req.logout
+        req.logout();
+        //should be blank
+        res.send('user: ' + req.user);
+    });
+
     app.get('/api/current_user', (req, res) => {
+        //session extracts data from cookie and assigns to req.session with passport key-value pair. Passport then takes info from req.session (cookie => session => passport)
+        //res.send(req.session)
+
         //route handler to test and see if user is present
         res.send(req.user);
     });
