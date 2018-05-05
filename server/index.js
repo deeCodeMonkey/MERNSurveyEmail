e1bdf08e996313d771b447e65e559211263a52d3
deeCodeMonkey <deidra108@gmail.com>
2018-05-05 13:08:27 -0600

    heroku settings for client build

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 //execute to assigned const
-const PORT = process.env.PORT || 5000;
 const keys = require('./config/keys');
 
 const express = require('express');
@@ -38,7 +37,18 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 
+//heroku production setting
+if (process.env.NODE_ENV === 'production') {
+    //Express to serve up production assets like main.js or main.css file in the React build folder
+    //if cannot find route, look in client-build (a React-Router page)
+    app.use(express.static('client/build'));
+    //Express will serve up index.html if it doesn't recognize the route (default to index.hmtml)
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
