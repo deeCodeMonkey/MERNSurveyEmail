a81ae08f53854e99160fbc234c85b914192b6af9
deeCodeMonkey <deidra108@gmail.com>
2018-05-06 20:44:33 -0600

    list of surveys

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,5 +1,5 @@
 ﻿import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 //action creator
 //export const fetchUser = () => {
@@ -30,4 +30,9 @@ export const submitSurvey = (values, history) => async dispatch => {
     history.push('/surveys');
     dispatch({ type: FETCH_USER, payload: res.data });
 
+};
+
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
---
+++ server/client/src/reducers/surveyReducer.js
@@ -0,0 +1,12 @@
+﻿import { FETCH_SURVEYS } from '../actions/types';
+
+//return action payload (accessible by mapStateToProps in react components)
+export default function (state = [], action) {
+    //console.log('surveyreducer: ', action);
+    switch (action.type) {
+        case FETCH_SURVEYS:
+            return action.payload || false;
+        default:
+            return state;
+    }
+}
